97d819e623213fd30cf0b0efb06d2bdc17d5c116
balandaaa <balandaaa@gmail.com>
2016-10-16 19:16:48 +0200

    camera controller kész

---
+++ ArenaOfOrcs/Assets/Script/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    void Start()
+    {
+    }
+
+
+    void LateUpdate()
+    {
+        Vector3 position = transform.position;
+        position.x = target.transform.position.x ;
+        //position.y = target.transform.position.y;
+        transform.position = position;
+    }
+}
